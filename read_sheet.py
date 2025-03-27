40df53a91ba7f5b731f59e0dfd431d1813f4201c
Neeraj Ravi <ravineerajk@gmail.com>
2025-03-27 16:04:29 +0530

    Fix: load GOOGLE_CREDS from Streamlit secrets

--- read_sheet.py
+++ read_sheet.py
@@ -1,17 +1,22 @@
 import gspread
+import os
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-import json
-import os
 
-def read_google_sheet(sheet_name, worksheet_name, creds_path="creds.json"):
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/drive"]
-    creds_dict = json.loads(os.environ[GOOGLE_CREDS])
+def read_google_sheet(sheet_name, worksheet_name):
+    # Use Streamlit secrets (this works locally and on Streamlit Cloud)
+    from streamlit import secrets
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    # Load from Streamlit secrets (as JSON string)
+    creds_dict = json.loads(secrets["GOOGLE_CREDS"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
     client = gspread.authorize(creds)
-    
     sheet = client.open(sheet_name).worksheet(worksheet_name)
+
     data = sheet.get_all_records()
-    df = pd.DataFrame(data)
-    return df
+    return pd.DataFrame(data)
